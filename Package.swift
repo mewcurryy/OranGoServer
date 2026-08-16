40908ed715c862f7c4a8f088311353753011a25f
Davin P <davinpidada2005@gmail.com>
2026-08-16 11:45:48 +0700

    fix: fix device deployment version bug

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "OranGoServer",
     platforms: [
-       .macOS(.v13)
+       .macOS(.v13),
+       .iOS(.v13)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
