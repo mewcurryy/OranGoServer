3f044c33577bab1803d69f6e357202202ef4d1d0
Davin P <davinpidada2005@gmail.com>
2026-08-17 13:31:07 +0700

    fix(migration): remove non-existent unique constraint drop in AlterAturanThreshold

--- Sources/OranGoServer/Migrations/AlterAturanThresholdRemoveGrade.swift
+++ Sources/OranGoServer/Migrations/AlterAturanThresholdRemoveGrade.swift
@@ -10,7 +10,7 @@ import Fluent
 struct AlterAturanThresholdRemoveGrade: AsyncMigration {
     func prepare(on database: any Database) async throws {
         try await database.schema("aturan_threshold")
-            .deleteUnique(on: "retail_grade_id", "grade_id")
+//            .deleteUnique(on: "retail_grade_id", "grade_id")
             .deleteField("grade_id")
             .unique(on: "retail_grade_id")
             .update()
